9a2f74512f0595ad03abd164f73ef6aa088199a7
jonathan schiebel <jonathan.schiebel@gmail.com>
2015-01-08 10:51:32 +0100

    Modification Toolbox Modification core
    Codes sans erreurs mais core est ? modifier avec scrapy

--- candidatecrawler/core/toolbox.py
+++ candidatecrawler/core/toolbox.py
@@ -1,14 +1,67 @@
 '''
 Created on 7 janv. 2015
 
-@author: Julie S
+@author: Jonathan
 '''
-from xml.etree import ElementTree
+### External modules importation ###
+
+import os
+import datetime
+import http.client
+import xml.etree.ElementTree
+
+### End of external modules importation ###
+
+### Functions ###
+
+def timestamp():
+    """Create a unique time stamp"""
+    now = datetime.datetime.now().strftime("%Y%m%d%H%M%S") # We get current date and format it YYYYMMDDHHMMSS
+
+    return now
+
+def current_date():
+    """Get current date, and format it"""
+    currentdate = datetime.date.today().strftime("%d-%m-%Y") # We get current date and format it DD-MM-YYYY
+
+    return currentdate
+
+def compute_duration(day,month,year):
+    """Compute duration between 2 dates"""
+    duration = datetime.datetime.now() - datetime.datetime(year,month,day)
+
+    return duration.days
+
+def ping_website(url):
+    """Ping a website"""
+    website_status = True
+
+    try:
+        conn = http.client.HTTPConnection(url)
+        conn.request("GET",url)
+    except:
+        website_status = False
+
+    return website_status
+
+def html_reader(domainName,uri):
+    """Read an HTML page"""
+    conn = http.client.HTTPConnection(domainName)
+    conn.request("GET",uri)
+    r1 = conn.getresponse()
+
+    if r1.reason != "OK":
+        print("{0} - {1}".format(r1.status, r1.reason))
+        print(r1.getheader('Location'))
+
+    htmlpage = r1.read()
+
+    return htmlpage
 
 def xml_reader(input_file, entry):
     """Read datas from an XML file"""
     xmlfile = open(input_file, "r", encoding="utf-8")
-    tree = ElementTree.ElementTree()
+    tree = xml.etree.ElementTree.ElementTree()
     tree.parse(xmlfile)
     root = tree.getroot()
 
@@ -18,3 +71,21 @@ def xml_reader(input_file, entry):
 
     return askedfield
 
+def xml_writer(input_file, output_file, entry_value_dict, backup=True):
+    """Write datas to an XML file"""
+    xmlfile = open(input_file, "r", encoding="utf-8")
+    tree = xml.etree.ElementTree.ElementTree()
+    tree.parse(xmlfile)
+    root = tree.getroot()
+
+    for key, value in entry_value_dict.items():
+        root.find(key).text = value
+
+    tree.write(output_file, encoding="utf-8", xml_declaration=True)
+    xmlfile.close()
+
+    if backup:
+        os.remove(input_file)
+        os.rename(output_file,input_file)
+
+### End of functions ###
