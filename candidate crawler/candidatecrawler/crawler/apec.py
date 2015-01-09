d5be77241f6ac424ae4a5b85b40a47f8a7e0bf1a
pierre <pierre@teilhard.name>
2015-01-09 17:03:19 +0100

    Correction de init spider

--- candidate crawler/candidatecrawler/crawler/apec.py
+++ candidate crawler/candidatecrawler/crawler/apec.py
@@ -10,6 +10,8 @@ from scrapy.selector import Selector
 
 from scrapy.spider import BaseSpider
 from _cffi_backend import callback
+from twisted.python.formmethod import Password
+from time import sleep
 
 
 class ApecItem(Item):
@@ -21,6 +23,9 @@ class CVSpider(BaseSpider):
     name = "CVFinder"
     allowed_domains = ["recruteurs.apec.fr"]
     start_urls = ['http://recruteurs.apec.fr/Accueil/ApecIndexAccueil.jsp?PEGA_HREF_950420318_0_0_doLogin=doLogin']
+    password = ""
+    login = ""
+    critere = {}
     
     def __init__(self, login, password, critere):
         self.critere = critere
@@ -29,28 +34,31 @@ class CVSpider(BaseSpider):
         
 
     def parse(self, response):
-        
-        formdata = {'PEGA_IMBT_12928340_0_0_doLogin':login,'PEGA_IMBT_24835201_0_0_doLogin':password}
+         
+        formdata = {'PEGA_IMBT_12928340_0_0_doLogin':self.login,'PEGA_IMBT_24835201_0_0_doLogin':self.password}
         print("parse")
         yield FormRequest(url="http://recruteurs.apec.fr/Accueil/ApecIndexAccueil.jsp",
-                                  method="POST",
+                                  method="GET",
                                   formdata=formdata,
                                   callback=self.parseB
                                   )
+        
 
     def parseB(self,response):
         print("parseB")
         yield FormRequest(url="http://recruteurs.apec.fr/Accueil/ApecIndexAccueil.jsp?PEGA_HRSM_704371172_0_0_doGoConsulterCV=doGoConsulterCV"
                           ,method="POST",
                           callback=self.parseC)
+    
         
     def parseC(self,response):
         print('parseC')#dans la methode suivante la partie entre HRSM_ et le premier _0 ne sert a rien et peut etre remplacee par n importe quoi
         yield FormRequest(url="http://recruteurs.apec.fr/CV/Candidapec/ApecCreateRequete.jsp?PEGA_HRSM_i<3consept_0_0_doSearchByCriteres=doSearchByCriteres"
                           ,method="POST"
-                          ,formdata=self.formulaire
+                          #,formdata=self.formulaire
                           #formdata = {'PEGA_TXFD_109774019_0_keywords':'catia' ,}
                           ,callback=self.parseD) #TODO rajouter les criteres
+     
     
     def parseD(self,response):
         print("parseD")
@@ -58,7 +66,7 @@ class CVSpider(BaseSpider):
         res = list()
         print(response)
         print(response)
-        rows = hxs.xpath("//a[@id = 'unique-id']/@href")
+        rows = hxs.xpath("//div[@id='chenillard']")
         print(len(rows))
         print(len(rows))
         for row in rows:
