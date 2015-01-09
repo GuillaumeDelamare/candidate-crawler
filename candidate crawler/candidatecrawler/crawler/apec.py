d2278c1013163dbadd30df7e6e49bfeefdbf11f4
pierre <pierre@teilhard.name>
2015-01-09 11:57:14 +0100

    tentative de multispider avec des basespiders

--- candidate crawler/candidatecrawler/crawler/apec.py
+++ candidate crawler/candidatecrawler/crawler/apec.py
@@ -9,7 +9,7 @@ from scrapy.item import Item, Field
 from scrapy.selector import Selector
 
 from scrapy.spider import BaseSpider
-from twisted.python.util import println
+from _cffi_backend import callback
 
 
 class ApecItem(Item):
@@ -17,7 +17,7 @@ class ApecItem(Item):
     print("bouh")
 
 
-class CVSpider(BaseSpider):
+class LogSpider(BaseSpider):
     name = "CVFinder"
     allowed_domains = ["recruteurs.apec.fr"]
     start_urls = ['http://recruteurs.apec.fr/Accueil/ApecIndexAccueil.jsp?PEGA_HREF_950420318_0_0_doLogin=doLogin']
@@ -27,23 +27,86 @@ class CVSpider(BaseSpider):
         
         formdata = {'PEGA_IMBT_12928340_0_0_doLogin':'47179308','PEGA_IMBT_24835201_0_0_doLogin':'6KPA43V8'}
         print("tata")
-        yield FormRequest(url="http://recruteurs.apec.fr/CV/Candidapec/ApecCreateRequete.jsp?PEGA_HRSM_1851182314_0_0_doSearchByCriteres=doSearchByCriteres",
+        yield FormRequest(url="http://recruteurs.apec.fr/Accueil/ApecIndexAccueil.jsp",
                                   method="POST",
                                   formdata=formdata,
-                                  callback=self.parse_page,
+                                  callback=ClickSpider.parse,
                                   )
-                #spider
+        
 
 
-    def parse_page(self, response):
+    def parse_page(self, response):         
+        #hxs = Selector(response)
+        print("yolo")
+# 
+#         rows = hxs.xpath('//a[contains(@href,"CV")]')
+#         print(len(rows))
+#         for row in rows:
+#             item = ApecItem()
+#             url = row.extract()
+#             item['urlCV'] = url
+#             print(url) 
+#             print(url)
+#             yield item
+
+class ClickSpider(BaseSpider):
+            name = "click"
+            allowed_domains = ["recruteurs.apec.fr"]
+            start_urls = ['http://recruteurs.apec.fr/Accueil/ApecIndexAccueil.jsp']
+            
+            def parse(self,response):
+                print("ClickSpider")
+                print("ClickSpider")
+                yield FormRequest(url = "http://recruteurs.apec.fr/Accueil/ApecIndexAccueil.jsp?PEGA_HRSM_704371172_0_0_doGoConsulterCV=doGoConsulterCV",
+                                  method="POST"
+                                  ,callback=CritereSpider.parse)
+        
+            def parse_page(self,response):
+                print("boss")
+                
+                
+                
+class CritereSpider(BaseSpider):
+    name = "criteres"
+    allowed_domains = ["recruteurs.apec.fr"]
+    start_urls = ['http://recruteurs.apec.fr/Accueil/ApecIndexAccueil.jsp?PEGA_HRSM_704371172_0_0_doGoConsulterCV=doGoConsulterCV']
+            
+    def parse(self,response):
+       
+        print("CritereSpider")
+        print("CritereSpider") 
+        yield FormRequest(url = "http://recruteurs.apec.fr/CV/Candidapec/ApecCreateRequete.jsp?PEGA_HRSM_1739906055_0_0_doSearchByCriteres=doSearchByCriteres"
+                          ,method="POST"
+                          ,callback=self.parse_page) #rajouter les criteres (voir formdata de LogSpider)
+        
+    def parse_page(self,response):
         hxs = Selector(response)
-
-        rows = hxs.xpath('//a')
+        res = []
+        
+        rows = hxs.xpath('//td[@class="titreCV"]/dl/dt/a/text()')
+        print(len(rows))
         print(len(rows))
         for row in rows:
-            print("juste apres")
             item = ApecItem()
-            url = row.extract()[0]
+            url = row.extract()
             item['urlCV'] = url
-            print("toto")
-            yield item
+            print(url) 
+            print(url)
+            res = res.append(item)
+            
+        print(len(res))
+        
+        
+# class DLSpider(BaseSpider):
+#     name = "downloader"
+#     allowed_domains = ["recruteurs.apec.fr"]
+#     start_urls = ['http://recruteurs.apec.fr/CV/Candidapec/ApecCreateRequete.jsp?PEGA_HRSM_1739906055_0_0_doSearchByCriteres=doSearchByCriteres']
+#     
+#     def parse(self,response):
+#         yield FormRequest(url)
+    
+    
+    
+    
+    
+    
