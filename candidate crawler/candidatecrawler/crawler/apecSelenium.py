36aaff40b34d4eecd778853d34b0b23839aa17cb
jonathan schiebel <jonathan.schiebel@gmail.com>
2015-01-13 16:56:48 +0100

    Avancement apecSpider

--- candidate crawler/candidatecrawler/crawler/apecSelenium.py
+++ candidate crawler/candidatecrawler/crawler/apecSelenium.py
@@ -9,7 +9,7 @@ class apecSelenium(InitSpider):
     baseurl = "http://recruteurs.apec.fr/Accueil/ApecIndexAccueil.jsp?PEGA_HREF_950420318_0_0_doLogin=doLogin"
     login = "47179308"
     password = "6KPA43V8"
-    keyword = "catia"
+    keyword = ""
     region = []
     mobilite = ""
     salaire = ""
@@ -29,30 +29,33 @@ class apecSelenium(InitSpider):
         self.disponibilite = disponibilite
         self.fraicheur = fraicheur
         self.nombreCV = nombreCV
-        self.driver = webdriver.Firefox()
+        
+        
     
     def parse(self, response):
-        
-        self.driver.get(self.baseurl)
+        """"Lancement du driver"""
+        driver = webdriver.Firefox()
+        #driver.set_window_position(-2000, -3000)
+        driver.get(self.baseurl)
         
         """Login"""
-        self.driver.find_element_by_id('login').clear()
-        self.driver.find_element_by_id('login').send_keys(self.login)
-        self.driver.find_element_by_id('password').clear()
-        self.driver.find_element_by_id('password').send_keys(self.password) 
-        login = self.driver.find_element_by_css_selector('.boutonSmall.floatRight.marginRight.noMarginBottom>a')
-        mouseLogin = webdriver.ActionChains(self.driver)
+        driver.find_element_by_id('login').clear()
+        driver.find_element_by_id('login').send_keys(self.login)
+        driver.find_element_by_id('password').clear()
+        driver.find_element_by_id('password').send_keys(self.password) 
+        login = driver.find_element_by_css_selector('.boutonSmall.floatRight.marginRight.noMarginBottom>a')
+        mouseLogin = webdriver.ActionChains(driver)
         mouseLogin.move_to_element(login).click().perform()
         
         """Acces a la page de recherche"""
-        buttonCV = self.driver.find_elements_by_css_selector('.button1>a')
-        mouseRecherche = webdriver.ActionChains(self.driver)
+        buttonCV = driver.find_elements_by_css_selector('.button1>a')
+        mouseRecherche = webdriver.ActionChains(driver)
         mouseRecherche.move_to_element(buttonCV[2]).click().perform()
         
         """Remplissage du formulaire"""
         """Mot-clef"""
-        self.driver.find_element_by_id('autocomplete').clear()
-        self.driver.find_element_by_id('autocomplete').send_keys(self.keyword)
+        driver.find_element_by_id('autocomplete').clear()
+        driver.find_element_by_id('autocomplete').send_keys(self.keyword)
    
         """Region"""
         #TODO
@@ -69,10 +72,18 @@ class apecSelenium(InitSpider):
         """Fraicheur"""
         #TODO
         
-        """nombreCV"""
+        """Acces a la page des CVs"""
+        bouttonRechercher = driver.find_element_by_css_selector('.button-search>a')
+        mouseAccesCVS = webdriver.ActionChains(driver)
+        mouseAccesCVS.move_to_element(bouttonRechercher).click().perform()
+        
+        """Enregistrer les liens des CVs a telecharger"""
+        #Todo
+        
+        """Telecharger les Cvs"""
         #TODO
         
-        """Acces a la page des CVs"""
-        bouttonRechercher = self.driver.find_element_by_css_selector('.button-search>a')
-        mouseAccesCVS = webdriver.ActionChains(self.driver)
-        mouseAccesCVS.move_to_element(bouttonRechercher).click().perform()
+        """Fermeture de la fenetre"""
+        driver.close()
+        
+        
