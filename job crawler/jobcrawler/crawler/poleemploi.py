99a413010690f2283ea7a484614c0e6f182d09cc
guillaume delamare <guillaume.delamare@gmail.com>
2015-01-16 10:29:33 +0100

    Fix the pole emploi crawler

--- job crawler/jobcrawler/crawler/poleemploi.py
+++ job crawler/jobcrawler/crawler/poleemploi.py
@@ -21,67 +21,47 @@ from jobcrawler.core import toolbox
 
 ### Classes ###
 class PoleemploiCrawler(object):
+    def __init__(self):
+        self.webdomain = "candidat.pole-emploi.fr"
+        self.regions = {"Toute la France": "PAYS_01",
+                        "Alsace": "REGION_42",
+                        "Aquitaine": "REGION_72",
+                        "Auvergne": "REGION_83",
+                        "Basse-Normandie": "REGION_25",
+                        "Bourgogne": "REGION_26",
+                        "Bretagne": "REGION_53",
+                        "Centre": "REGION_24",
+                        "Champagne-Ardenne": "REGION_21",
+                        "Franche-Comté": "REGION_43",
+                        "Haute-Normandie": "REGION_23",
+                        "Ile-de-France": "REGION_11",
+                        "Languedoc-Roussillon": "REGION_91",
+                        "Limousin": "REGION_74",
+                        "Lorraine": "REGION_41",
+                        "Midi-Pyrénées": "REGION_73",
+                        "Nord-Pas-de-Calais": "REGION_31",
+                        "Pays de la Loire": "REGION_52",
+                        "Picardie": "REGION_22",
+                        "Poitou-Charentes": "REGION_54",
+                        "PACA": "REGION_93",
+                        "Rhône-Alpes": "REGION_82"}
 
     def _poleemploi_crawler(self, keywords, daterange, region):
         """Crawler for Pole emploi"""
         site_list = []
-        webdomain = "candidat.pole-emploi.fr"
-
-        if not toolbox.ping_website(webdomain):
-            print("{0} not responding".format(webdomain))
+        
+        if not toolbox.ping_website(self.webdomain):
+            print("{0} not responding".format(self.webdomain))
             return
 
-        if region == "Toute la France":
-            region_code = "PAYS_01"
-        elif region == "Alsace":
-            region_code = "REGION_42"
-        elif region == "Aquitaine":
-            region_code = "REGION_72"
-        elif region == "Auvergne":
-            region_code = "REGION_83"
-        elif region == "Basse-Normandie":
-            region_code = "REGION_25"
-        elif region == "Bourgogne":
-            region_code = "REGION_26"
-        elif region == "Bretagne":
-            region_code = "REGION_53"
-        elif region == "Centre":
-            region_code = "REGION_24"
-        elif region == "Champagne-Ardenne":
-            region_code = "REGION_21"
-        elif region == "Franche-Comté":
-            region_code = "REGION_43"
-        elif region == "Haute-Normandie":
-            region_code = "REGION_23"
-        elif region == "Ile-de-France":
-            region_code = "REGION_11"
-        elif region == "Languedoc-Roussillon":
-            region_code = "REGION_91"
-        elif region == "Limousin":
-            region_code = "REGION_74"
-        elif region == "Lorraine":
-            region_code = "REGION_41"
-        elif region == "Midi-Pyrénées":
-            region_code = "REGION_73"
-        elif region == "Nord-Pas-de-Calais":
-            region_code = "REGION_31"
-        elif region == "Pays de la Loire":
-            region_code = "REGION_52"
-        elif region == "Picardie":
-            region_code = "REGION_22"
-        elif region == "Poitou-Charentes":
-            region_code = "REGION_54"
-        elif region == "PACA":
-            region_code = "REGION_93"
-        elif region == "Rhône-Alpes":
-            region_code = "REGION_82"
+        region_code = self.regions[region]
 
         for keyword in keywords:
-            uri = "/candidat/rechercheoffres/resultats/A_{0}_{1}__11___________INDIFFERENT______________{2}".format(keyword, region_code, daterange)
-            soup = bs4.BeautifulSoup(toolbox.html_reader(webdomain,uri))
+            uri = "/candidat/rechercheoffres/resultats/A__{1}___P________{0}__INDIFFERENT______________{2}___".format(keyword, region_code, daterange)
+            soup = bs4.BeautifulSoup(toolbox.html_reader(self.webdomain,uri))
             for link in soup.find_all(href=re.compile("rechercheoffres")):
                 link = re.sub(("\;(.*?)$"),'',link.get("href")).replace("resultats.composantresultatrechercheoffre.tableauresultatrechercheoffre:detailoffre","detail")
-                site_list.append("http://{0}{1}".format(webdomain,link))
+                site_list.append("http://{0}{1}".format(self.webdomain,link))
 
         site_list = list(set(site_list))
 
@@ -99,6 +79,6 @@ class PoleemploiCrawler(object):
 
 if __name__=='__main__':
     runapp = PoleemploiCrawler()
-    print(runapp.run_program())
+    print(runapp.run_program(["Java"], 3, "Pays de la Loire"))
 
 ### End of Main program ###
